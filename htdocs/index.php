cca40b0a36a9f81f1e11844162820e226fc838b3
yfg2014 <2324704583@qq.com>
2014-06-14 21:38:30 +0800

    change the file of index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -4,7 +4,7 @@
 /* ini_set('error_reporting', E_ALL); //打开所有的错误级别
 	ini_set('display_errors', 1); //显示错误 */
 	define("_START_", true);
-	define("_APP_PATH", "./source/"); // app file path
+	define("_APP_PATH", "../source/"); // app file path
 	require_once(_APP_PATH."Front.comm.php");
 	$s = new App();
 	$s->Run();
